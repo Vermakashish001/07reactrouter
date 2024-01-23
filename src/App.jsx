2a3662fc92b7d29109ded97d120106f2d9ccb093
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-01-23 22:00:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-600 p-4'>React router</h1> 
+    </>
+  )
+}
+
+export default App
